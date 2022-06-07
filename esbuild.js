a98d6ebfe3cd8c6b3c51cb3cd4ccf72d1a05ac17
Arturo Sanchez <asanchez@orthocana.com>
2022-06-06 19:39:31 -0500

    # EXAMPLES/TEXTURES
    Signed-off-by: Arturo Sanchez <asanchez@orthocana.com>

--- esbuild.js
+++ esbuild.js
@@ -6,6 +6,6 @@ esbuild.build({
     sourcemap : true,
     target : 'es2015',
     minify : true,
-    outfile: './dist/08/app.js',
+    outfile: './dist/09/app.js',
     tsconfig: './tsconfig.json'
 }).catch(() => process.exit(1));
